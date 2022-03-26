7a4ac908b52f1532e0c402a3d91a894317a3bd41
Serdar <serdar@gmail.com>
2022-03-27 00:33:49 +0100

    files added

---
+++ src/components/item/Item.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./Item.css";
+
+const Item = ({ card }) => {
+  const [showLogo, setShowLogo] = useState(true);
+
+  return (
+    <div className="card" onClick={() => {
+       setTimeout(() => setShowLogo(!showLogo), 0);
+       setTimeout(() => setShowLogo(showLogo), 4000);
+      
+      }}>
+      {showLogo ? (
+        <div>
+          <img className="card-logo" src={card.img} alt="logo" />
+          <h3 className="card-title">{card.name}</h3>
+        </div>
+      ) : (
+        <ul className="list">
+          {card.options.map((element, index) => {
+            return <li key={index}>{element}</li>;
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Item;
+
+
+
+
