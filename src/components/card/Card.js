bd987d648ca35d255d6fd054d1ec8e8a347f1213
Serdar <serdar@gmail.com>
2022-03-28 00:15:39 +0200

    bar css

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -5,7 +5,7 @@ import "./Card.css";
 const Card = () => {
   return (
     <div className="cards-area-container">
-      {/* <div className="bars"></div> */}
+      <div className="bars"></div>
       <h1 className="language-title">Languages</h1>
       <div className="cards-container">
         {languages.map((item, index) => {
